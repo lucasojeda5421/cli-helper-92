d82e46e7ed38f84c26bd6a9e69c1619e14274eef
Sander Ojeda <lucasojeda5421@gmail.com>
2026-08-29 09:08:48 +0200

    memoized hash calculations for crypto

--- helpers.ts
+++ helpers.ts
@@ -1,43 +1,36 @@
-import * as crypto from 'crypto';
-
-export class CryptoOptimizer {
-  private hashCache: Map<string, string> = new Map();
-  private keyCache: Map<string, Buffer> = new Map();
-
-  computeHash(data: string, algorithm: string = 'sha256'): string {
-    const cacheKey = `${algorithm}:${data}`;
-    if (this.hashCache.has(cacheKey)) {
-      return this.hashCache.get(cacheKey)!;
-    }
-    const hash = crypto.createHash(algorithm).update(data).digest('hex');
-    if (this.hashCache.size > 1000) {
-      const firstKey = this.hashCache.keys().next().value;
-      this.hashCache.delete(firstKey);
-    }
-    this.hashCache.set(cacheKey, hash);
-    return hash;
-  }
-
-  deriveKey(password: string, salt: string, iterations: number = 10000): Buffer {
-    const cacheKey = `${password}:${salt}:${iterations}`;
-    if (this.keyCache.has(cacheKey)) {
-      return this.keyCache.get(cacheKey)!;
-    }
-    const key = crypto.pbkdf2Sync(password, salt, iterations, 32, 'sha512');
-    if (this.keyCache.size > 100) {
-      const firstKey = this.keyCache.keys().next().value;
-      this.keyCache.delete(firstKey);
-    }
-    this.keyCache.set(cacheKey, key);
-    return key;
-  }
-
-  batchComputeHashes(datas: string[]): string[] {
-    return datas.map(data => this.computeHash(data));
-  }
-
-  clearCaches(): void {
-    this.hashCache.clear();
-    this.keyCache.clear();
-  }
+interface CryptoCache {
+get(input: string): string;
+batchGet(inputs: string[]): string[];
+clear(): void;
 }
+
+const cache = new Map<string, string>();
+
+function hash(input: string): string {
+let h = 0;
+for (let i = 0; i < input.length; i++) {
+h = (h << 5) - h + input.charCodeAt(i);
+h = h & h;
+}
+return (h >>> 0).toString(16);
+}
+
+const helpers: CryptoCache = {
+get(input: string): string {
+if (cache.has(input)) {
+return cache.get(input)!;
+}
+const result = hash(input);
+cache.set(input, result);
+return result;
+},
+batchGet(inputs: string[]): string[] {
+return inputs.map((input) => helpers.get(input));
+},
+clear(): void {
+cache.clear();
+}
+};
+
+export default helpers;
+export { helpers, CryptoCache };
